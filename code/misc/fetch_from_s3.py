84f82e4799cf8afdf1a4f6e755727280b583020f
Rushabh Jain <jainrushabh65@gmail.com>
2023-09-26 15:48:28 +0200

    Code cleanup

--- code/misc/fetch_from_s3.py
+++
@@ -1,9 +0,0 @@
-from utils.s3_tools import fetch_data
-
-if __name__ == '__main__':
-    remote_dir = 'lr/data/ct/'
-    source = remote_dir + 'source/'
-    labels = remote_dir + 'labels/'
-    
-    fetch_data(source, '../data/source')
-    fetch_data(labels, '../data/labels')
